7a0e9f07de59cad3f68204d29e2a50a5efb4e715
galKatzav <galoosh1591@gmail.com>
2024-07-12 22:30:14 +0300

    Loan Class Update

--- src/model/Loan.java
+++ src/model/Loan.java
@@ -4,25 +4,33 @@ import java.util.Date;
 
 public class Loan {
     private Book book;
+    private Member member;
     private Date loanDate;
     private Date returnDate;
 
-    public Loan(Book book) {
+    public Loan(Book book, Member member) {
         this.book = book;
+        this.member = member;
         this.loanDate = new Date();
-        this.returnDate = null;
     }
+
     public Book getBook() {
         return book;
     }
+
+    public Member getMember() {
+        return member;
+    }
+
     public Date getLoanDate() {
         return loanDate;
     }
+
     public Date getReturnDate() {
         return returnDate;
     }
-    public void returnBook() {
-        this.returnDate = new Date();
-        book.setAvailable(true);
+
+    public void setReturnDate(Date returnDate) {
+        this.returnDate = returnDate;
     }
 }
