4dce06f17a3f2e73fdda219a6be88a0f49ad49a1
galKatzav <galoosh1591@gmail.com>
2024-07-12 18:30:28 +0300

    RatedBook update

--- src/decorator/RatedBook.java
+++ src/decorator/RatedBook.java
@@ -1,4 +1,20 @@
 package decorator;
 
-public class RatedBook {
+import model.Book;
+
+public class RatedBook extends BookDecorator{
+    private double rating;
+
+    public RatedBook(Book decoratedBook, double rating) {
+        super(decoratedBook);
+        this.rating = rating;
+    }
+
+    public double getRating() {
+        return rating;
+    }
+    public void setRating(double rating) {
+        this.rating = rating;
+    }
+
 }
