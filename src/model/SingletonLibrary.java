bbf48a92ca60af3a1288fc98070a958bcec6cd1e
galKatzav <galoosh1591@gmail.com>
2024-07-12 18:30:43 +0300

    SingletonLibrary update

--- src/model/SingletonLibrary.java
+++ src/model/SingletonLibrary.java
@@ -1,4 +1,15 @@
 package model;
 
-public class SingletonLibrary {
+public class SingletonLibrary extends Library{
+    private static SingletonLibrary instance;
+
+    private SingletonLibrary() {
+        super();
+    }
+    public static SingletonLibrary getInstance() {
+        if (instance == null) {
+            instance = new SingletonLibrary();
+        }
+        return instance;
+    }
 }
