b7904fb1a9057fc18a498757c5737086dbe27603
galKatzav <galoosh1591@gmail.com>
2024-07-12 18:26:56 +0300

    BookObserver update

--- src/model/BookObserver.java
+++ src/model/BookObserver.java
@@ -1,4 +1,14 @@
 package model;
 
-public class BookObserver {
+public class BookObserver implements Observer{
+    private String name;
+
+    public BookObserver(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public void update(Book book) {
+        System.out.println("Notification to " + name + ": Book " + book.getTitle() + " is now " + (book.isAvailable() ? "available" : "not available"));
+    }
 }
