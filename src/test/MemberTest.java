0043e3c6ee8f44f99651a36eb2ef16250cb0c7c1
galKatzav <galoosh1591@gmail.com>
2024-07-12 22:48:16 +0300

    MemberTest File

---
+++ src/test/MemberTest.java
@@ -0,0 +1,33 @@
+package test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import model.Book;
+import model.Loan;
+import model.Member;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class MemberTest {
+
+    private Member member;
+
+    @BeforeEach
+    public void setUp() {
+        member = new Member("John Doe", "123");
+    }
+
+    @Test
+    public void testMemberInitialization() {
+        assertEquals("John Doe", member.getName());
+        assertEquals("123", member.getId());
+    }
+
+    @Test
+    public void testAddLoan() {
+        Loan loan = new Loan(new Book("The Catcher in the Rye", "J.D. Salinger", 1951), member);
+        member.addLoan(loan);
+        assertEquals(1, member.getLoans().size());
+    }
+}
+
