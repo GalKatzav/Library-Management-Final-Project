e904020da82dd327a771fee38f4ae3cb3907a5f8
galKatzav <galoosh1591@gmail.com>
2024-07-12 18:26:34 +0300

    BookDecorator update

--- src/decorator/BookDecorator.java
+++ src/decorator/BookDecorator.java
@@ -1,5 +1,25 @@
 package decorator;
 
-public class BookDecorator {
-//    BookDecorator
+import model.Book;
+
+
+public class BookDecorator extends Book{
+    protected Book decoratedBook;
+
+    public BookDecorator(Book decoratedBook) {
+        super(decoratedBook.getTitle(), decoratedBook.getAuthor(), decoratedBook.getYear());
+        this.decoratedBook = decoratedBook;
+    }
+    @Override
+    public String getTitle() {
+        return decoratedBook.getTitle();
+    }
+    @Override
+    public String getAuthor() {
+        return decoratedBook.getAuthor();
+    }
+    @Override
+    public int getYear() {
+        return decoratedBook.getYear();
+    }
 }
