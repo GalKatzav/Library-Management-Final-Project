c2e2efac63f09cda9777296ba98eb8af9b7b105a
galKatzav <galoosh1591@gmail.com>
2024-07-12 18:27:36 +0300

    ConcreteLibraryFactory update

--- src/model/ConcreteLibraryFactory.java
+++ src/model/ConcreteLibraryFactory.java
@@ -1,4 +1,10 @@
 package model;
 
-public class ConcreteLibraryFactory {
+public class ConcreteLibraryFactory extends LibraryFactory{
+    public Book createBook(String title, String author, int year) {
+        return new Book(title, author, year);
+    }
+    public Member createMember(String name, String id) {
+        return new Member(name, id);
+    }
 }
