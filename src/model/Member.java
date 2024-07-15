c110c5a035b9098202de9c69ef4e99050654a61e
galKatzav <galoosh1591@gmail.com>
2024-07-15 14:45:15 +0300

    Member File - Comments

--- src/model/Member.java
+++ src/model/Member.java
@@ -6,12 +6,32 @@ import DesingP.observer.Observer;
 import java.util.ArrayList;
 import java.util.List;
 
+
+/**
+ * The {@code Member} class represents a member of the library.
+ * It contains information about the member's name, ID, and loans.
+ * The class also supports observer pattern to notify observers of changes.
+ */
 public class Member extends Observable {
+
+    /** The name of the member. */
     private String name;
+
+    /** The ID of the member. */
     private String id;
+
+    /** The list of loans that the member has. */
     private List<Loan> loans;
+
+    /** The list of observers observing this member. */
     private List<Observer> observers;
 
+    /**
+     * Constructs a new {@code Member} object with the specified name and ID.
+     *
+     * @param name The name of the member.
+     * @param id The ID of the member.
+     */
     public Member(String name, String id) {
         this.name = name;
         this.id = id;
@@ -19,22 +39,57 @@ public class Member extends Observable {
         this.observers = new ArrayList<>();
     }
 
+    /**
+     * Returns the name of the member.
+     *
+     * @return The name of the member.
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the ID of the member.
+     *
+     * @return The ID of the member.
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Returns the list of loans that the member has.
+     *
+     * @return The list of loans.
+     */
+    public List<Loan> getLoans() {
+        return loans;
+    }
+
+    /**
+     * Adds a loan to the member's list of loans.
+     *
+     * @param loan The loan to add.
+     */
     public void addLoan(Loan loan) {
         loans.add(loan);
     }
 
+    /**
+     * Removes a loan from the member's list of loans.
+     *
+     * @param loan The loan to remove.
+     */
     public void removeLoan(Loan loan) {
         loans.remove(loan);
     }
 
+    /**
+     * Finds a loan by the book's title.
+     *
+     * @param title The title of the book.
+     * @return The loan corresponding to the book title, or {@code null} if not found.
+     */
     public Loan findLoanByBook(String title) {
         for (Loan loan : loans) {
             if (loan.getBook().getTitle().equals(title)) {
@@ -44,18 +99,29 @@ public class Member extends Observable {
         return null;
     }
 
-    public List<Loan> getLoans() {
-        return loans;
-    }
-
+    /**
+     * Adds an observer to the member.
+     *
+     * @param observer The observer to add.
+     */
     public void addObserver(Observer observer) {
         observers.add(observer);
     }
 
+    /**
+     * Removes an observer from the member.
+     *
+     * @param observer The observer to remove.
+     */
     public void removeObserver(Observer observer) {
         observers.remove(observer);
     }
 
+    /**
+     * Notifies all observers with a given message.
+     *
+     * @param message The message to send to observers.
+     */
     public void notifyObservers(String message) {
         for (Observer observer : observers) {
             observer.update(message);
