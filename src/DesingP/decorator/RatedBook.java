2b5aa9e251535ae94eb1980f3e97051656d04d9d
galKatzav <galoosh1591@gmail.com>
2024-07-15 15:03:42 +0300

    RatedBook File - Comments

--- src/DesingP/decorator/RatedBook.java
+++ src/DesingP/decorator/RatedBook.java
@@ -2,24 +2,62 @@ package DesingP.decorator;
 
 import model.Book;
 
+/**
+ * The {@code RatedBook} class is a decorator that extends the functionality of the {@link Book} class
+ * by adding a rating to the book.
+ */
 public class RatedBook extends BookDecorator{
+
+    /** The rating of the book. */
     private double rating;
+
+    /**
+     * Constructs a new {@code RatedBook} object.
+     *
+     * @param decoratedBook The book instance to be decorated.
+     * @param rating The rating of the book.
+     */
 
     public RatedBook(Book decoratedBook, double rating) {
         super(decoratedBook);
         this.rating = rating;
     }
 
+    /**
+     * Returns the rating of the book.
+     *
+     * @return The rating of the book.
+     */
     public double getRating() {
         return rating;
     }
+
+    /**
+     * Sets the rating of the book.
+     *
+     * @param rating The new rating of the book.
+     */
     public void setRating(double rating) {
         this.rating = rating;
     }
+
+    /**
+     * Returns the title of the decorated book along with its rating.
+     *
+     * @return The title of the book with its rating.
+     */
     @Override
     public String getTitle() {
         return decoratedBook.getTitle() + " (Rated: " + rating + ")";
     }
+
+    /**
+     * Compares this rated book with another object for equality.
+     * Two rated books are considered equal if their ratings and decorated books are equal.
+     *
+     * @param obj The object to compare to.
+     * @return {@code true} if the objects are equal, {@code false} otherwise.
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) return true;
