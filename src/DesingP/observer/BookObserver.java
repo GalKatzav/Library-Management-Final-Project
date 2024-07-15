02a7d49daa85aee9bc505732bd17764a0099bb69
galKatzav <galoosh1591@gmail.com>
2024-07-15 16:28:02 +0300

    BookObserver File - Comments

--- src/DesingP/observer/BookObserver.java
+++ src/DesingP/observer/BookObserver.java
@@ -1,14 +1,29 @@
 package DesingP.observer;
 
-import model.Book;
 
+/**
+ * The {@code BookObserver} class implements the {@link Observer} interface.
+ * It represents an observer that gets notified with messages about updates in the observed subject.
+ */
 public class BookObserver implements Observer {
+
+    /** The name of the observer. */
     private String name;
 
+    /**
+     * Constructs a new {@code BookObserver} object with the specified name.
+     *
+     * @param name The name of the observer.
+     */
     public BookObserver(String name) {
         this.name = name;
     }
 
+    /**
+     * Receives an update with a message and prints a notification to the console.
+     *
+     * @param message The message containing the update information.
+     */
     @Override
     public void update(String message) {
         System.out.println("Notification to " + name + ": " + message);
