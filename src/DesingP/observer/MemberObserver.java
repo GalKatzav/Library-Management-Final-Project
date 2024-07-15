c556027bd2091dc5ab66d41ca3d9a76618ffcff4
galKatzav <galoosh1591@gmail.com>
2024-07-15 16:41:19 +0300

    MemberObserver File - Comments

--- src/DesingP/observer/MemberObserver.java
+++ src/DesingP/observer/MemberObserver.java
@@ -1,12 +1,29 @@
 package DesingP.observer;
 
+
+/**
+ * The {@code MemberObserver} class implements the {@link Observer} interface.
+ * It represents an observer that gets notified with messages about updates in the observed subject.
+ */
 public class MemberObserver implements Observer {
+
+    /** The name of the observer. */
     private String name;
 
+    /**
+     * Constructs a new {@code MemberObserver} object with the specified name.
+     *
+     * @param name The name of the observer.
+     */
     public MemberObserver(String name) {
         this.name = name;
     }
 
+    /**
+     * Receives an update with a message and prints a notification to the console.
+     *
+     * @param message The message containing the update information.
+     */
     @Override
     public void update(String message) {
         System.out.println("Notification to " + name + ": " + message);
