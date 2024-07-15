9a4aa7d20300b23c64abfb61898f79bbf1626cb7
galKatzav <galoosh1591@gmail.com>
2024-07-15 16:51:05 +0300

    BookStateException File - Comments

--- src/DesingP/util/BookStateException.java
+++ src/DesingP/util/BookStateException.java
@@ -1,6 +1,17 @@
 package DesingP.util;
 
+/**
+ * The {@code BookStateException} class represents an exception that is thrown when there is
+ * an invalid state or operation related to a book in the library system. This exception
+ * extends the {@link Exception} class and provides a message describing the error.
+ */
 public class BookStateException extends Exception{
+
+    /**
+     * Constructs a new {@code BookStateException} with the specified detail message.
+     *
+     * @param message The detail message explaining the reason for the exception.
+     */
     public BookStateException(String message) {
         super(message);
     }
