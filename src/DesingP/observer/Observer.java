6691c63310ee129ca3e4de3f8e2c90a8bb0252a8
galKatzav <galoosh1591@gmail.com>
2024-07-15 16:45:45 +0300

    Observer File - Comments

--- src/DesingP/observer/Observer.java
+++ src/DesingP/observer/Observer.java
@@ -1,7 +1,16 @@
 package DesingP.observer;
 
-import model.Book;
-
+/**
+ * The {@code Observer} interface should be implemented by any class that wants to receive
+ * updates from an {@link Observable} object. It defines a single method, {@code update},
+ * which is called when the observable object wants to notify its observers of a change.
+ */
 public interface Observer {
+
+    /**
+     * Called when the observable object wants to notify the observer of a change.
+     *
+     * @param message The message containing information about the update.
+     */
     void update(String message);
 }
